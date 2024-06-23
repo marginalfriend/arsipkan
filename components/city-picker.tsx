51238711e9a6ce32b8e101b10553035b86c78658
marginalfriend <abdullahpsg@gmail.com>
2024-06-23 23:20:24 +0700

    [CHANGES] revised after refactors

--- components/city-picker.tsx
+++ components/city-picker.tsx
@@ -1,11 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-
-import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -42,58 +37,43 @@ export function CityPicker(form: any) {
   // Fetching provinces data from GoAPI
   useEffect(() => {
     const provincesURL = "https://api.goapi.io/regional/provinsi";
-		
+
     fetch(provincesURL, {
       headers: new Headers({
-				"X-API-KEY": process.env.GOAPI_KEY ? process.env.GOAPI_KEY : "",
-			}),
+        "X-API-KEY": process.env.GOAPI_KEY ? process.env.GOAPI_KEY : "",
+      }),
     })
-    .then(res => res.json())
-		.then(data => setProvinces(data.data)),
-});
+      .then((res) => res.json())
+      .then((data) => setProvinces(data.data));
+  });
 
-	function fetchCities(province: any) {
-		fetch(citiesURL + `?provinsi_id=${province}`, {
-			headers: new Headers({
-      "X-API-KEY": process.env.GOAPI_KEY ? process.env.GOAPI_KEY : "",
-    }),
-		})
-		.then((res) => res.json())
-		.then((data) => {
-			console.log("This is from city fetch")
-			console.log(data)
-			setCities(data.data);
-		});
-	}
+  const citiesURL = "https://api.goapi.io/regional/kota";
+  function fetchCities(province: string) {
+    fetch(citiesURL + `?provinsi_id=${province}`, {
+      headers: new Headers({
+        "X-API-KEY": process.env.GOAPI_KEY ? process.env.GOAPI_KEY : "",
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("This is from city fetch");
+        console.log(data);
+        setCities(data.data);
+      });
+  }
 
-  const [cities, setCities]: [City[], Function] = useState([]);
-
-	// Fetching cities data from GoAPI
+  // Fetching cities data from GoAPI
   useEffect(() => {
     "use server";
 
-		const citiesURL = "https://api.goapi.io/regional/kota"
-
-		fetch(citiesURL, {
-			headers: new Headers({
-				"X-API-KEY": "2c40acf8-fa47-56c5-f83e-b34d7ccc"
-			})
-		})
-		.then(res => res.json())
-		.then(data => setCities(data.data))
-
+    fetch(citiesURL, {
+      headers: new Headers({
+        "X-API-KEY": "2c40acf8-fa47-56c5-f83e-b34d7ccc",
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => setCities(data.data));
   }, []);
-
-  function onSubmit(data: z.infer<typeof FormSchema>) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    });
-  }
 
   return (
     <FormField
@@ -112,7 +92,10 @@ export function CityPicker(form: any) {
               <SelectContent>
                 {provinces?.map((province) => {
                   return (
-                    <SelectItem key={province.id} value={province.id.toString()}>
+                    <SelectItem
+                      key={province.id}
+                      value={province.id.toString()}
+                    >
                       {province.name}
                     </SelectItem>
                   );
@@ -125,7 +108,7 @@ export function CityPicker(form: any) {
             </FormDescription>
             <FormMessage />
           </FormItem>
-					
+
           <FormItem>
             <FormLabel>Kota</FormLabel>
             <Select onValueChange={field.onChange} defaultValue={field.value}>
