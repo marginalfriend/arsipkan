c2aaf6a9990e5d3999872715034a11c3cb658879
marginalfriend <abdullahpsg@gmail.com>
2024-06-23 16:56:44 +0700

    [CHANGES] refactoring header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,25 +1,35 @@
 import Link from "next/link";
 import AuthButton from "./auth-button";
 
+const routes = [
+	{
+		href: '/',
+		label: 'Home'
+	},
+	{
+		href: '/receipt',
+		label: 'Receipt'
+	}
+]
+
 export function Header() {
 	return(
 	<header className="flex flex-row w-screen items-center justify-between px-10 py-2 border-b mb-6">
 		<div className="flex">
 			<ul className="list-none flex gap-2">
-				<li>
-					<Link href={'/'}>
-						<div className="p-2 hover:bg-slate-300 hover:cursor-pointer rounded-md hover:font-semibold">
-							Home
-						</div>
-					</Link>
-				</li>
-				<li>
-					<Link href={'/receipt'}>
-						<div className="p-2 hover:bg-slate-300 hover:cursor-pointer rounded-md hover:font-semibold">
-							Receipt
-						</div>
-					</Link>
-				</li>
+				{
+					routes.map((route) => {
+						return (
+							<li key={route.href}>
+								<Link href={'/'}>
+									<div className="p-2 hover:bg-slate-300 hover:cursor-pointer rounded-md font-semibold">
+										{route.label}
+									</div>
+								</Link>
+							</li>
+						)
+					})
+				}
 			</ul>
 		</div>
 		<AuthButton />
