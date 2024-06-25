8d4e59c55e6996ae151964809cc60a8f9e36d8dd
marginalfriend <abdullahpsg@gmail.com>
2024-06-25 15:30:25 +0700

    [NEW] columns definition

--- app/project/columns.tsx
+++ app/project/columns.tsx
@@ -1,10 +1,43 @@
 "use client"
 
+import { ColumnDef } from "@tanstack/react-table"
+
 export type Project = {
-	id: string
 	spkNumber: string
 	clientName: string
 	projectName: string
-	value: number
 	date: Date
+	city: string
+	value: number
+	remaining: number
 }
+
+export const columns: ColumnDef<Project>[] = [
+	{
+		accessorKey: "spkNumber",
+		header: "Nomor SPK",
+	},
+	{
+		accessorKey: "clientName",
+		header: "Nama Klien",
+	},	{
+		accessorKey: "projectName",
+		header: "Nama Projek",
+	},
+	{
+		accessorKey: "city",
+		header: "Kota Projek",
+	},
+	{
+		accessorKey: "date",
+		header: "Tanggal SPK",
+	},
+	{
+		accessorKey: "value",
+		header: "Nilai Projek",
+	},
+	{
+		accessorKey: "remaining",
+		header: "Yang Belum Dibayar",
+	},
+]
