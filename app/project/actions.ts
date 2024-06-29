c2b757cd8890a17eaffe2d2a3f9669b0ed80b709
marginalfriend <abdullahpsg@gmail.com>
2024-06-29 14:47:20 +0700

    [NEW] include city name

--- app/project/actions.ts
+++ app/project/actions.ts
@@ -11,14 +11,22 @@ export async function getProjects() {
 	
 	try {
 
-		(await prisma.sPK.findMany({})).forEach(data => {
+		(await prisma.sPK.findMany({
+			include: {
+				city: {
+					select: {
+						name: true,
+					}
+				}
+			}
+		})).forEach(data => {
 			result.push(
 				{
 					spkNumber: data.number,
 					clientName: data.clientCompanyName,
 					projectName: data.projectName,
 					date: data.date.toLocaleDateString('id-ID', {day: 'numeric', month: 'short', year: 'numeric'}),
-					city: data.cityId,
+					city: data.city.name,
 					value: data.value,
 					remaining: data.value
 				}
