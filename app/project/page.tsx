62d875c5580a5a075018fab983a0071b4507b42e
marginalfriend <abdullahpsg@gmail.com>
2024-07-10 09:35:49 +0700

    [NEW] page header

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -8,8 +8,11 @@ import { CreateProjectDialog } from "./components/create-project-dialog";
 
 export default function Page() {
   return (
-    <main className="flex flex-col items-center justify-center gap-10 lg:px-4 m-0 mb-24 max-w-[100vw]">
-      <CreateProjectDialog />
+    <main className="flex flex-col items-center justify-center gap-4 lg:px-4 m-0 mb-24 max-w-[100vw]">
+      <div className="flex w-full justify-between">
+				<h1 className="text-3xl font-bold">Projek</h1>
+        <CreateProjectDialog />
+      </div>
       <ProjectTable />
     </main>
   );
--- app/receipt/page.tsx
+++ app/receipt/page.tsx
@@ -3,8 +3,12 @@ import { CreateKwitansiDialog } from "./components/create-kwitansi-dialog";
 export default function Page() {
   return (
     <>
-      <main className="flex flex-col items-center justify-center gap-10 px-4 mb-24">
-        <CreateKwitansiDialog />
+      <main className="flex flex-col items-center justify-center gap-4 lg:px-4 m-0 mb-24 max-w-[100vw]">
+        <div className="flex w-full justify-between">
+          <h1 className="text-3xl font-bold">Projek</h1>
+          <CreateKwitansiDialog />
+        </div>
+				
       </main>
     </>
   );
